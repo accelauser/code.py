5c121513267da96d89c5b93e2eec6b5f2d657ac0
accelauser <bespg100@gmail.com>
2024-01-15 22:50:52 -0300

    Mudei o nome :) funciona melhor

--- game_of_life/main.py
+++ game_of_life/main.py
@@ -1,9 +1,7 @@
 #preciso refatorar mas vai assim mesmo 
 import random
 import time
-import numpy
-import curses
-
+import curses # bagulho pra fazer os icones aparecem no mesmo lugar no terminal, funciona sem ele mas precisa de alterações 
 stdscr = curses.initscr()
 
 def my_raw_input(stdscr, r, c, prompt_string): #https://stackoverflow.com/questions/21784625/how-to-input-a-word-in-ncurses-screen
@@ -14,31 +12,27 @@ def my_raw_input(stdscr, r, c, prompt_string): #https://stackoverflow.com/questi
     input = stdscr.getstr(r + 1, c, 20)
     return input  #       ^^^^  reading input at next line  
 
-def makeGrid(): #cria a matrix e importa padrões
-    #grid_size = int(input("Grid size: "))
-    grid_size = int(my_raw_input(stdscr, 0,0, "Grid size: "))
-    grid = [[0 for y in range(grid_size)] for x in range(grid_size)]
-    #pattern = int(input("Select a pattern (0 for none: )"))
+def makeGrid(grid_h,grid_w): #cria a matrix e importa padrões
+    grid = [[0 for y in range(grid_w)] for x in range(grid_h)]
     pattern = int(my_raw_input(stdscr, 0,0, "Number of pattern (2 all random): "))
     match pattern:
         case 0:
             pass
-
         case 1:
-            grid[(grid_size//2)-1][grid_size//2] = 1
-            grid[grid_size//2][grid_size//2] = 1
-            grid[(grid_size//2)+1][grid_size//2] = 1
+            grid[(grid_h//2)-1][grid_w//2] = 1
+            grid[grid_h//2][grid_w//2] = 1
+            grid[(grid_h//2)+1][grid_w//2] = 1
         case 2: 
-            grid = [[random.randint(0,1) for y in range(grid_size)] for x in range(grid_size)]
+            grid = [[random.randint(0,1) for y in range( grid_w)] for x in range(grid_h)]
     return grid
 
 def printMatrix(matrix): #printa as matrix direito
     for x in matrix:
         print(x)
 
-def printSim(matrix): #printa as matrix de símbo
+def printSim(matrix): #printa as matrix de símbolos
     stdscr.clear()
-    for x in range(len(matrix)) :
+    for x in range(grid_w): #não entendi pq so funciona com grid_w
         stdscr.addstr(x, 0, ( ' '.join(matrix[x])))
         stdscr.refresh()
 
@@ -62,9 +56,9 @@ def count_neighbors(grid, row, col): #conta o número de vizinhos vivos (chatgpt
 
 def evolution(grid): #itera sobre a matrix e muda o estado das células baseado na rules() e joga essa informações em uma nova lista
     new_grid = []
-    for x in range(len(grid)):
+    for x in range(grid_h):
         new_grid.append([])
-        for y in range(len(grid)):
+        for y in range(grid_w):
             new_grid[x].append(rules(grid[x][y],count_neighbors(grid, x, y)))
     return new_grid
 
@@ -116,6 +110,16 @@ def generations(grid,generations): #cria uma nova matrix baseada na anterior da 
             x += 1
             time.sleep(1)
 
-#generations(makeGrid(), int(input('Number of generations: ')))
-generations(makeGrid(), int(my_raw_input(stdscr, 0,0, "Number of generations: ")))
+#preciso das variaveis disponiveis globalmente :)
+grid_size = int(my_raw_input(stdscr, 0,0, "Custom size (1 or 0): "))
+if grid_size != 0: 
+    grid_h = int(my_raw_input(stdscr, 0,0, "Grid height "))
+    grid_w = int(my_raw_input(stdscr, 0,0, "Grid width: ")) +2
+else:
+    grid_w, grid_h = stdscr.getmaxyx()
 
+def main():
+    generations(makeGrid(grid_h, grid_w), int(my_raw_input(stdscr, 0,0, "Number of generations: ")))
+
+if __name__ == '__main__':
+    main()
