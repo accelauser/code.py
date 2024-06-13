fbea56f22a66445fea237f9415b907d451d40703
accelauser <bespg100@gmail.com>
2024-06-12 22:46:48 -0300

    new project

--- physics/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-    def update(self):
-        self.tt.setheading(self.phi) #muda o ângulo da direção da trajetória        
-        self.tt.fd(self.v0) #anda pra frente    
-        self.vy = round(math.sin(math.radians(self.phi)) * self.v0) + self.g
-        self.vx = round((math.cos(math.radians(self.phi)) * self.v0))
